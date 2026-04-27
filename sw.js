8545252562400ea01e4c498422859bed55cfc105
akunaljain <53397517+akunaljain@users.noreply.github.com>
2026-04-27 10:10:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "fastcalc-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
