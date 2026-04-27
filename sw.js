bf777d4fabc418f670d7ac5c28980b596d6a3882
akunaljain <53397517+akunaljain@users.noreply.github.com>
2026-04-27 10:18:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,46 @@
-const CACHE_NAME = "fastcalc-v1";
+const CACHE_NAME = "fastcalc-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  self.skipWaiting(); // force install
+
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim(); // control immediately
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
